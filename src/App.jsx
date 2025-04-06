ae48544f6da47eeb14812e6def60e26012ac886e
krishsah01 <157822205+krishsah01@users.noreply.github.com>
2025-04-06 12:24:15 -0400

    Added login and modified gameview to display the username.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import RPS from "./pages/RockPaperScissors/RPS";
 import TicTacToe from "./pages/TicTacToe/TicTacToe";
 import HomePage from "./HomePage";
 import Navigation from "./components/Navigation";
-
-
+import Footer from "./components/Footer";
+import LoginPage from "./pages/Login/LoginPage";
 
 
 const App = () => {
@@ -15,10 +15,14 @@ const App = () => {
       <Navigation />
       <div className="app-container">
         <Routes>
+          <Route path="/login" element={<LoginPage />} />
           <Route index element={<HomePage />} />
           <Route path="/Rock-Paper-Scissors" element={<RPS/>} />
           <Route path="/tic-tac-toe" element={<TicTacToe />} />
         </Routes>
+      </div>
+      <div className="footer">
+        <Footer />
       </div>
     </BrowserRouter>
   );
