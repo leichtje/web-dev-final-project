19d12106f050e96d9963b1700aff44ec52d58f7f
RaviNayak-cell <nayakrr@mail.uc.edu>
2025-04-15 14:13:10 -0400

    hangman edits

--- src/pages/Hangman/Hangman.jsx
+++ src/pages/Hangman/Hangman.jsx
@@ -13,7 +13,7 @@ class Hangman extends Component {
   static defaultProps = {
     maxWrong: 6,
     images: [img0, img1, img2, img3, img4, img5, img6],
-    wordLength: 5, // Default word length
+    wordLength: 5, 
   };
 
   constructor(props) {
@@ -21,7 +21,7 @@ class Hangman extends Component {
     this.state = {
       nWrong: 0,
       guessed: new Set(),
-      answer: "", // Initialize empty answer
+      answer: "", 
     };
 
     this.handleGuess = this.handleGuess.bind(this);
@@ -41,7 +41,7 @@ class Hangman extends Component {
       this.setState({ answer: data[0] });
     } catch (error) {
       console.error("Error fetching word:", error);
-      this.setState({ answer: "error" }); // Fallback word in case of failure
+      this.setState({ answer: "error" });
     }
   }
 
